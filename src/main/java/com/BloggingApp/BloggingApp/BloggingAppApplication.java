22b510526ae9a73ef27e9a64081cabe06eda1c3d
Rohit <rohit11joshi10@gmail.com>
2026-04-14 03:00:35 +0530

    Disabled Redis repo scanning to fix startup crash

--- src/main/java/com/BloggingApp/BloggingApp/BloggingAppApplication.java
+++ src/main/java/com/BloggingApp/BloggingApp/BloggingAppApplication.java
@@ -10,12 +10,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.BloggingApp.BloggingApp.repositories")
+// Isse Redis repositories ki scanning bypass ho jayegi
+@EnableRedisRepositories(includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {}))
 @EnableAsync
 public class BloggingAppApplication implements CommandLineRunner {
 
